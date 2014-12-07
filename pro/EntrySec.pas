c7f103ca146581505b48499ce9491d3286c247b6
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2014-12-07 12:53:32 +0300

    version updated

--- pro/EntrySec.pas
+++ pro/EntrySec.pas
@@ -262,7 +262,7 @@ begin
   FMenu.CurrentUserRoles := q.FieldByName('Roles_Ident').AsInteger;
   FMenu.CurrentUserName := eShortName.text;
   bAllData := ChBoxAll.Checked;
-  version := '3.0.01.12.14';
+  version := '3.0.06.12.14';
   period := iff(bAllData, 'ÂÑÅ ÂÐÅÌß', '6 Måñÿöåâ');
   // other tables
   account_table_other := iff(not bAllData, 'account_all', 'account');
